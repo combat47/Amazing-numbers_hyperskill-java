46e3071a83afe85488d70043ab46d614262adc5f
Combat47 <115223808+combat47@users.noreply.github.com>
2023-10-01 09:22:30 +0330

    Main.java

--- Stage 5 - Spy numbers/Main.java
+++ Stage 5 - Spy numbers/Main.java
@@ -1,176 +1,82 @@
 package numbers;
 
-import java.util.*;
+import java.util.Collections;
+import numbers.Number.*;
 
-import static numbers.Number.Properties.*;
+public class Main {
+    public static void main(String[] args) {
+        //write your code here
+        String welcome = "Welcome to Amazing Numbers!\n";
+        String description = "Supported requests:\n" +
+                "- enter a natural number to know its properties;\n" +
+                "- enter two natural numbers to obtain the properties of the list:\n" +
+                "  * the first parameter represents a starting number;\n" +
+                "  * the second parameter shows how many consecutive numbers are to be printed;\n" +
+                "- two natural numbers and property to search for;\n" +
+                "- separate the parameters with one space;\n" +
+                "- enter 0 to exit.";
 
-public class UserInput {
-    private Scanner scan;
-    private final static String requestMessage = "Enter a request: ";
-    private String[] values;
-    private int size;
-    private Long number;
-    private int quantity;
-    private Set<Number.Properties> inclusiveProperties;
-    private Set<Number.Properties> exclusiveProperties;
-    private List<String> wrongProperties;
-    private List<String> muatualExclusiveProperties;
+        UserInput userInput = new UserInput();
 
-    UserInput() {
-        this.scan = new Scanner(System.in);
-    }
+        Number number;
 
-    public int getSize() {
-        return size;
-    }
+        System.out.println(welcome);
+        System.out.println(description);
 
-    public Long getNumber() {
-        return number;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public Set<Number.Properties> getInclusiveProperties() {
-        return inclusiveProperties;
-    }
-
-    public Set<Number.Properties> getExclusiveProperties() {
-        return exclusiveProperties;
-    }
-
-    /**
-     * Scans the user input, splits it and stores it in the String array values.
-     * The number entered by the user is stores as Long in the field number, but not validated.
-     * @return this
-     */
-    UserInput scan() {
-        System.out.print(requestMessage);
-        this.values = scan.nextLine().split(" ");
-        size = values.length;
-        this.number = Long.parseLong(values[0]);
-        return this;
-    }
-
-    /**
-     * Validates the user input.
-     * If the inputs are valid, the method stores the values in the corresponding fields, except the number.
-     * @return true, if all user inputs are valid
-     */
-
-    boolean isValid() {
-
-        if (number < 0) {
-            System.out.println("The first parameter should be a natural number or zero.");
-            return false;
-        }
-
-        // validates the entered quantity and stores it in the corresponding field if valid
-        if (values.length > 1) {
-            if (Long.parseLong(values[1]) < 1) {
-                System.out.println("The second parameter should be a natural number");
-                return false;
-            } else {
-                quantity = Integer.parseInt(values[1]);
-            }
-        }
-
-        // initiates the collections for the properties entered by the user and generates error messages based on the entered properties
-        if (values.length > 2) {
-            inclusiveProperties = new HashSet<>();
-            exclusiveProperties = new HashSet<>();
-            wrongProperties = new ArrayList<>();
-            muatualExclusiveProperties = new ArrayList<>();
-            String errorMessage;
-
-            sortProperties();
-
-            if (wrongProperties.size() > 0) {
-                errorMessage = wrongProperties.size() > 1 ? "The properties %s are wrong.\n" : "The property %s is wrong.\n";
-                System.out.printf(errorMessage, wrongProperties);
-                System.out.printf("Available properties: %s\n", Arrays.toString(values()));
-                return false;
-            }
-
-            findMutalExclusive();
-
-            if (muatualExclusiveProperties.size() > 0) {
-                System.out.print("The request contains mutually exclusive properties: ");
-                System.out.println(muatualExclusiveProperties);
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * validates the entered properties and sorts them in different collections: inclusive, exlusive or wrong
-     */
-    private void sortProperties() {
-        boolean exclusive;
-        Number.Properties property;
-        String value;
-
-        makePropertiesUpperCase();
-
-        for (int i = 2; i < values.length; i++) {
-
-            // if the user entes a property with the prefix "-", the property is exclusive. The variable stores that knowlegde
-            exclusive = values[i].charAt(0) == '-' ? true : false;
-
-            // if the property is exclusive, the prefix "-" is removed
-            if (exclusive) {
-                value = values[i].substring(1);
-            } else {
-                value = values[i];
-            }
-
-            // check if the entered property is a valid property. If not, the value ist stored in the wrongProperties collection.
-            // otherwise the value is stored in the corresponding collection (inclusive or exclusive)
-            if (exists(value)) {
-                if (exclusive) {
-                    exclusiveProperties.add(valueOf(value));
+        do {
+            if (userInput.scan().isValid()) {
+                if (userInput.getNumber() == 0) {
+                    System.out.println("Goodbye!");
+                    userInput.close();
                 } else {
-                    inclusiveProperties.add(valueOf(value));
+                    number = new Number(userInput.getNumber());
+                    number.setProperties();
+                    if (userInput.getSize() < 2) {
+                        System.out.println(listPropertiesVertically(number));
+                    } else if (userInput.getSize() < 3) {
+                        for (int i = 0; i < userInput.getQuantity(); i++) {
+                            System.out.println(listPropertiesInRow(number));
+                            number = new Number(number.VALUE + 1).setProperties();
+                        }
+                    } else if (userInput.getSize() >= 3) {
+                        int count = 0;
+                        while (count < userInput.getQuantity()) {
+                            // test whether all inclusive properties of the request are included in the number's properties
+                            boolean containsAll = number.getProperties().containsAll(userInput.getInclusiveProperties());
+                            // test whether all exclusive properties of the request are not present int the number's properties
+                            boolean excludesAll = Collections.disjoint(number.getProperties(), userInput.getExclusiveProperties());
+                            // if both tests are true, the number's properties are printed
+                            if (containsAll && excludesAll) {
+                                System.out.println(listPropertiesInRow(number));
+                                count++;
+                            }
+                            number = new Number(number.VALUE + 1).setProperties();
+                        }
+                    }
                 }
-            } else {
-                wrongProperties.add(value);
             }
-        }
+        } while (userInput.getNumber() != 0);
     }
 
-    /**
-     * makes all entered properties uppercase. This way, they can be easier used together with the enum Properties.
-     */
-    private void makePropertiesUpperCase() {
-        for (int i = 2; i < values.length; i++) {
-            values[i] = values[i].toUpperCase();
+    static String listPropertiesVertically(Number number) {
+        StringBuilder str = new StringBuilder();
+
+        str.append("Properties of ").append(number.VALUE).append("\n");
+
+        for (Properties value : Properties.values()) {
+            str.append(value).append(": ").append(number.getProperties().contains(value)).append("\n");
         }
+
+        return str.toString();
     }
 
-    /**
-     * searches for mutual exlusive properties in the collections. If properties are mutual exclusive, they are stored in the collection muatualExclusiveProperties.
-     */
-    private void findMutalExclusive() {
-        for (Number.Properties property : inclusiveProperties) {
-            if (property.mutualExclusive != "" && inclusiveProperties.contains(valueOf(property.mutualExclusive))) {
-                muatualExclusiveProperties.add(property.toString());
-            }
-            if (exclusiveProperties.contains(property)) {
-                muatualExclusiveProperties.add(property.toString());
-                muatualExclusiveProperties.add("-" + property.toString());
-            }
-        }
+    static String listPropertiesInRow(Number number) {
+        StringBuilder properties = new StringBuilder();
 
-        for (Number.Properties property : exclusiveProperties) {
-            if (property.mutualExclusive != "" && exclusiveProperties.contains(valueOf(property.mutualExclusive))) {
-                muatualExclusiveProperties.add("-" + property.toString());
-            }
+        for (Properties property : number.getProperties()) {
+            properties.append(property).append(", ");
         }
-    }
-
-    void close() {
-        scan.close();
+        properties.deleteCharAt(properties.lastIndexOf(","));
+        return number.VALUE + " is " + properties;
     }
 }
